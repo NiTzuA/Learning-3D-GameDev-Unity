ac2049aeaf94534c13c2f077d1845b0c03c4d468
NiTzuA <11v35faustin@gmail.com>
2023-06-21 15:01:48 +0800

    v1.04
    I actually didn't watch a tutorial on these:
    - Separated the velocity for the X and Z movement
    - Added acceleration, rather than just instant velocity change
    - Adjusted sideways movement when sprinting
    - Added hold to jump

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -9,10 +9,12 @@ public class MovementController : MonoBehaviour
     public Camera playerFOV;
 
     public float walkFOV = 80;
-    public float speed = 10f;
+    public float speed = 5f;
     public float sprintSpeed = 8.5f;
     public float gravity = -9.81f;
     public float jumpHeight = 5f;
+    float gameSpeedX;
+    float gameSpeedY;
 
     public Transform groundCheck;
     public float groundDistance = 0.4f;
@@ -24,13 +26,13 @@ public class MovementController : MonoBehaviour
     private void Start()
     {
         playerFOV.fieldOfView = walkFOV;
+        gameSpeedX = 0;
+        gameSpeedY = 0;
+
     }
 
     void Update()
     {
-
-        float gameSpeed = speed;
-
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
         if (isGrounded && velocity.y < 0) 
@@ -41,20 +43,39 @@ public class MovementController : MonoBehaviour
         float x = Input.GetAxisRaw("Horizontal");
         float z = Input.GetAxisRaw("Vertical");
 
-        Vector3 move = Vector3.Normalize(transform.right * x + transform.forward * z);
-
-        if (Input.GetKey(KeyCode.LeftShift))
+        if (x != 0f) 
         {
-            gameSpeed = sprintSpeed;
-            playerFOV.fieldOfView = Mathf.Lerp(playerFOV.fieldOfView, walkFOV + (walkFOV * 0.15f), 15f * Time.deltaTime); //walkFOV + (walkFOV * 0.15f);
+            gameSpeedX = Mathf.Lerp(gameSpeedX, speed, 3f * Time.deltaTime);
+        } else
+        {
+            gameSpeedX = Mathf.Lerp(gameSpeedX, 0, 3f * Time.deltaTime);
+        }
+
+        if (z != 0f)
+        {
+            gameSpeedY = Mathf.Lerp(gameSpeedY, speed, 3f * Time.deltaTime);
+        }
+        else
+        {
+            gameSpeedY = Mathf.Lerp(gameSpeedY, 0, 3f * Time.deltaTime);
+        }
+
+        Vector3 moveX = Vector3.Normalize(transform.right * x);
+        Vector3 moveY = Vector3.Normalize(transform.forward * z);
+
+        if (Input.GetKey(KeyCode.LeftShift) && Input.GetAxisRaw("Vertical") != 0)
+        {
+            gameSpeedY = sprintSpeed;
+            playerFOV.fieldOfView = Mathf.Lerp(playerFOV.fieldOfView, walkFOV + (walkFOV * 0.15f), 15f * Time.deltaTime); 
         } else
         {
             playerFOV.fieldOfView = Mathf.Lerp(playerFOV.fieldOfView, walkFOV, 15f * Time.deltaTime);
         }
 
-        controller.Move(move * gameSpeed * Time.deltaTime);
+        controller.Move(moveX * gameSpeedX * Time.deltaTime);
+        controller.Move(moveY * gameSpeedY * Time.deltaTime);
 
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if(Input.GetButton("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
